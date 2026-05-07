60ac6cf582365af385f5896c615ed584b2b95450
Arakis14 <wiktor.lasota@vp.pl>
2026-05-07 17:23:12 +0200

    Conan and SDL3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <print>
+#include <SDL3/SDL.h>
 
 int main()
 {
     std::println("Hello");
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow(
+        "Hello SDL3",
+        800,
+        600,
+        0
+    );
+
+    SDL_Delay(2000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
