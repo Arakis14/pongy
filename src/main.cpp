8fec159131406d7e33496c5d8b242e34c15de209
Arakis14 <wiktor.lasota@vp.pl>
2026-05-08 14:02:38 +0200

    Widndow with bmp rendered and simple keypress for quit

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,60 @@
-#include <print>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+const char* brickBG = "../assets/brick.bmp";
 
 int main()
 {
-    std::println("Hello");
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Surface *surface;
+    SDL_Texture *texture;
+    SDL_Event event;
 
-    if (!SDL_Init(SDL_INIT_VIDEO))
-    {
-        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-        return 0;
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 3;
     }
 
-    SDL_Window* window = SDL_CreateWindow(
-        "Hello SDL3",
-        800,
-        600,
-        0
-    );
-
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
-        return 1;
+    if (!SDL_CreateWindowAndRenderer("Hello SDL", 1024, 768, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
+        return 3;
     }
 
-    SDL_Delay(5000);
+    surface = SDL_LoadBMP(brickBG);
+    if (!surface) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create surface from image: %s", SDL_GetError());
+        return 3;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (!texture) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
+        return 3;
+    }
+    SDL_DestroySurface(surface);
 
+    bool quit_the_app = false;
+    while (!quit_the_app) {
+        SDL_PollEvent(&event);
+        if (event.type == SDL_EVENT_QUIT) {
+            quit_the_app = true;
+        }
+        if (event.type == SDL_EVENT_KEY_DOWN) {
+            /* the pressed key was Escape? */
+            SDL_Log("Key pressed");
+            if (event.key.key == SDLK_ESCAPE) {
+                quit_the_app = true;
+            }
+        }
+        SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
+        SDL_RenderClear(renderer);
+        SDL_RenderTexture(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+
     SDL_Quit();
 
     return 0;
