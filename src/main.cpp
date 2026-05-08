e4ef99c72a6f3d88660f66b236e5d6502f654c51
Arakis14 <wiktor.lasota@vp.pl>
2026-05-08 13:46:39 +0200

    Simple window

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 int main()
 {
     std::println("Hello");
-    SDL_Init(SDL_INIT_VIDEO);
+
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 0;
+    }
 
     SDL_Window* window = SDL_CreateWindow(
         "Hello SDL3",
@@ -13,7 +18,13 @@ int main()
         0
     );
 
-    SDL_Delay(2000);
+    if (window == NULL) {
+        // In the case that the window could not be made...
+        SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Delay(5000);
 
     SDL_DestroyWindow(window);
     SDL_Quit();
