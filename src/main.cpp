d4a12f0d5e2186d91450b6a98e9bd1cddc233df1
Arakis14 <wiktor.lasota@vp.pl>
2026-05-13 10:30:47 +0200

    added deltatime and fixed paddle going over the screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,49 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <cstdint>
 #include <cstdlib>
-#include <ctime>
+
 const char* brickBG = "../assets/brick.bmp";
+//userinput speed?
+const uint8_t speedMulti = 1;
+double deltaTime = 0;
+const uint16_t resolutionHeight = 1024;
+const uint16_t resolutionWidth = 768;
+
+double checkHeight(SDL_FRect& paddle)
+{
+    return paddle.y;
+}
 
 void  increaseHeight(SDL_FRect& paddle)
 {
-    paddle.y -= 2;
+    paddle.y -= 2 * speedMulti * deltaTime;
     //DEBUG
-    //SDL_Log("New paddle y: %f, paddle.y);
+    //SDL_Log("New paddle y: %f", paddle.y);
 }
 
 void  decreaseHeight(SDL_FRect& paddle)
 {
-    paddle.y += 2;
+    paddle.y += 2 * speedMulti * deltaTime;
     //DEBUG
-    //SDL_Log("New paddle y: %f, paddle.y);
+    //SDL_Log("New paddle y: %f", paddle.y);
 }
 
 int main()
 {
-    std::srand((unsigned int)std::time(nullptr));
     SDL_Window *window;
     SDL_FRect paddle1 = {50, 50, 50, 100};
     SDL_Renderer *renderer;
     SDL_Texture *texture;
     SDL_Event event;
-
+    Uint64 NOW = SDL_GetPerformanceCounter();
+    Uint64 LAST = 0;
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
         return 3;
     }
 
-    if (!SDL_CreateWindowAndRenderer("Hello SDL", 1024, 768, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("Hello SDL", resolutionHeight, resolutionWidth, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window and renderer: %s", SDL_GetError());
         return 3;
     }
@@ -40,6 +51,10 @@ int main()
     bool quit_the_app = false;
     texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1024, 768);
     while (!quit_the_app) {
+        LAST = NOW;
+        NOW = SDL_GetPerformanceCounter();
+
+        deltaTime = (double)((NOW - LAST)*1000 / (double)SDL_GetPerformanceFrequency() );
         SDL_PollEvent(&event);
         if (event.type == SDL_EVENT_QUIT) {
             quit_the_app = true;
@@ -52,13 +67,16 @@ int main()
             }
             else if(event.key.key == SDLK_DOWN || event.key.key == SDLK_S)
             {
-                decreaseHeight(paddle1);
+                if(checkHeight(paddle1) < resolutionWidth - paddle1.h) {
+                    decreaseHeight(paddle1);
+                }
             }
             else if(event.key.key == SDLK_UP || event.key.key == SDLK_W)
             {
-                increaseHeight(paddle1);
+                if(checkHeight(paddle1) > 0.00) {
+                    increaseHeight(paddle1);
+                }
             }
-
         }
         SDL_SetRenderTarget(renderer, texture);
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
